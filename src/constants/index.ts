51954bec93b881813d68e2a1fad40bb0f016e1de
David Rodriguez <59021247+F13-Kr1pt0n@users.noreply.github.com>
2024-04-05 22:26:37 -0500

    Add files via upload

---
+++ src/constants/index.ts
@@ -0,0 +1,34 @@
+export const sectionDefaultValues = {
+  sectionNumber: '',
+  classId: '',
+  meetingDays: '',
+  startTime: '',
+  endTime: '',
+  startDate: new Date(),
+  endDate: new Date(),
+  roomNumber: '',
+  instructorId: '',
+}
+
+export const userDefaultValues = {
+  clerkId: '',
+  email: '',
+  username: '',
+  firstName: '',
+  lastName: '',
+  role: '',
+}
+
+export const newUserDefaultValues = {
+  email: '',
+  username: '',
+  firstName: '',
+  lastName: '',
+  password: '',
+}
+export const assignmentDefaultValues = {
+  name: '',
+  dueTime: new Date(),
+  description: '',
+  maxPoints: 100,
+}
